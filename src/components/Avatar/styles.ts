1362834219d675f97856f7639d48b59df88a8760
Rogério Marques <rogerrm@live.com>
2023-05-23 23:06:08 -0300

    changes: add gradient border to Avatar Image like figma layout

--- src/components/Avatar/styles.ts
+++ src/components/Avatar/styles.ts
@@ -2,6 +2,9 @@ import { styled } from '@stitches/react'
 
 export const AvatarContainer = styled('img', {
   borderRadius: '$full',
+  objectFit: 'contain',
+  border: '1px solid transparent',
+  background: '$gradient-vertical',
 
   variants: {
     size: {
