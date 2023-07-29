2156463318523d715cee3cb81fd87012b9de4002
Rogério Marques <rogerrm@live.com>
2023-07-29 15:05:17 -0300

    refactor: baseURL environment variable | File .env changes

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,5 +1,10 @@
 import axios from 'axios'
 
+const baseURL =
+  process.env.NODE_ENV === 'production'
+    ? process.env.URL_PRODUCTION
+    : process.env.URL_DEV
+
 export const api = axios.create({
-  baseURL: '/api',
+  baseURL: `${baseURL}/api`,
 })
