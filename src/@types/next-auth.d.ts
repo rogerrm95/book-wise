d8a702ed379ecb5376958fb6801d869769a47829
Rogério Marques <rogerrm@live.com>
2023-07-29 13:46:22 -0300

    change: eslint change on Rules

--- src/@types/next-auth.d.ts
+++ src/@types/next-auth.d.ts
@@ -1,3 +1,4 @@
+// eslint-disable-next-line no-unused-vars
 import NextAuth from 'next-auth'
 
 declare module 'next-auth' {
@@ -8,6 +9,7 @@ declare module 'next-auth' {
     avatar_url: string
   }
 
+  // eslint-disable-next-line no-unused-vars
   interface Session {
     user: User
   }
